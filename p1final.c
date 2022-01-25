59e8c182b319e6dabb7931187607df0a8631a219
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-01-25 12:41:11 +0000

    debug completed

--- p1final.c
+++ p1final.c
@@ -4,7 +4,8 @@ int main()
   int num1,num2;
   float sum;
   printf("enter two numbers\n");
-  scanf("%d %d,"&num1,&num2);
+  scanf("%d %d",&num1,&num2);
   sum=num1+num2;
-  printf
+  printf("addition of%d+%d =%f\n",num1,num2,sum);
+  return 0;
 }
