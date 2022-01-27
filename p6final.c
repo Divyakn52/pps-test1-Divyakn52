34b14e737da26a78ad8b791bab39ffd3cb18a94b
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-01-27 09:30:51 +0000

    debug cmpltd

--- p6final.c
+++ p6final.c
@@ -3,13 +3,18 @@ int main()
 {
   int i, n, sum=0;
 
-  printf("\n enter a number\n");
-  scanf("%d" , &n);    //5
+   printf("\n enter a number\n");
+   scanf("%d" , &n);    //5
 
-  for(i=0;i<n; i++)     //i=5
-    {
+   for(i=0;i<n; i++)     //i=5
+     {
       sum=sum + i;
-    }
-  printf("/n sum ofn=%d\n",sum);
-  return 0;  
+     }
+
+    for(i=1; i<n; i++)   //i=5
+       {
+       printf("%d+\n",i);
+       }
+       printf("%d is %d\n",i,sum);
+     return 0;  
 }
