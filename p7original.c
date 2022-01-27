40e495ee2a3844de007e17e2a67709d78c65776b
Divzz <97013125+Divyakn52@users.noreply.github.com>
2022-01-27 10:48:41 +0000

    debug cmpltd

--- p7original.c
+++ p7original.c
@@ -2,8 +2,8 @@
 int input_array_size()
 {
   int k;
-  printf("enter the size of array;\n");
-  sacnf("%d",&k);
+  printf("enter the size of array:\n");
+  scanf("%d", &k);
   return k;
 }
 void array_input(int n,int *a)
@@ -19,7 +19,24 @@ int sum_n_arrays(int n,int a[n])
   int sum=0;
   for(int i=0;i<n;i++)
   {
-    sum=sum+a[i]
+    sum=sum+a[i];
   }
-  
+  return sum;
 }
+ void output(int n,int a[n], int sum)
+ { 
+    for(int i=0;i<n-1;i++)
+    { 
+      printf("%d+",a[i]);
+    }
+    printf("%d=%d\n",a[n-1],sum);
+ }
+ int main()
+ {
+   int n,sum;
+   n=input_array_size();
+   int a[n];
+   array_input(n,a);
+   sum=sum_n_arrays(n,a);
+   output(n,a,sum);
+ }
